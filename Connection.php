ba56baeb9b0a3c6cda87c7a5ba149405ee1bf507
Ildar Bikmamatov <vistoyn@gmail.com>
2019-07-18 18:47:29 +0000

    connect, query, insert

--- Connection.php
+++ Connection.php
@@ -26,11 +26,12 @@ use Elberos\Core\Struct;
 class Connection extends Struct
 {
 	
-	protected $host;
-	protected $login;
-	protected $password;
-	protected $db;
-	protected $pdo;
+	protected $__host = "";
+	protected $__port = "";
+	protected $__login = "";
+	protected $__password = "";
+	protected $__db = "";
+	protected $__pdo = null;
 	
 	
 	
@@ -49,14 +50,37 @@ class Connection extends Struct
 	 */
 	function connect()
 	{
-		$pdo = new \PDO(
-			'mysql:host='.$this->host.';dbname='.$this->db, $this->login, $this->password, 
-			array(
-				\PDO::ATTR_PERSISTENT => false
-			)
-		);
-		$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-		$pdo->exec("set names utf8");
+		if ($this->__pdo) return $this;
+		
+		$last_error = "";
+		try
+		{
+			$str = 'mysql:host='.$this->host;
+			if ($this->port !== null) $str .= ':'.$this->port;
+			if ($this->dbname !== null) $str .= ';dbname='.$this->db;
+			$pdo = new \PDO(
+				'mysql:host='.$this->host.';dbname='.$this->db, $this->login, $this->password, 
+				array(
+					\PDO::ATTR_PERSISTENT => false
+				)
+			);
+			$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+			$pdo->exec("set names utf8");
+		}
+		catch (\PDOException $e)
+		{
+			$last_error = 'Failed connected to database!';
+		}
+		catch (\Excepion $e)
+		{
+			$last_error = $e->getMessage();
+		}
+		
+		if ($last_error)
+		{
+			throw new \Exception($last_error);
+		}
+		
 		return $this->copy([ "pdo"=>$pdo ]);
 	}
 	
@@ -67,8 +91,7 @@ class Connection extends Struct
 	 */
 	function query($sql, $arr)
 	{
-		global $pdo;
-		$st = $pdo->prepare($sql, array(\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY));
+		$st = $this->pdo->prepare($sql, array(\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY));
 		$st->execute($arr);
 		return $st;
 	}
@@ -80,10 +103,72 @@ class Connection extends Struct
 	 */
 	function getOne($sql, $arr)
 	{
-		$st = pdo_query($sql, $arr);
+		$st = $this->query($sql, $arr);
 		return $st->fetch(PDO::FETCH_ASSOC);
 	}
 	
+	
+	
+	/**
+	 * Execute sql query
+	 */
+	function insert($table_name, $data)
+	{
+		$keys = [];
+		$values = [];
+		foreach ($data as $key=>$val)
+		{
+			$keys[] = "`" . $key . "`";
+			$values[] = ":" . $key;
+		}
+		$sql = "insert into " . $table_name . 
+			" (" . implode(",",$keys) . ") values (" . implode(",",$values) . ")"
+		;
+		$st = $this->pdo->prepare($sql, array(\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY));
+		$st->execute($data);
+		return $st;
+	}
+	
+	
+	
+	
+	/**
+	 * Execute sql query
+	 */
+	function insert_or_update($table_name, $insert, $update)
+	{
+		$ins_keys = [];
+		$ins_values = [];
+		$upd_data = [];
+		$data = [];
+		
+		foreach ($insert as $key => $val)
+		{
+			$keys[] = "`".$key."`";
+			$values[] = ":".$key;
+			$data[ $key ] = $val;
+		}
+		foreach ($update as $key => $val)
+		{
+			$keys[] = "`" . $key . "`";
+			$values[] = ":" . $key;
+			$upd_data[] = "`".$key."` = :" . $key;
+			$data[ $key ] = $val;
+		}
+		
+		$sql = "insert into " . $table_name . 
+			" (" . implode(",",$keys) . ") " .
+			" values (" . implode(",",$values) . ") " .
+			(
+				(count($upd_data) > 0) ? 
+					" ON DUPLICATE KEY UPDATE " . implode(",", $upd_data) : ""
+			)
+		;
+		
+		$st = $this->pdo->prepare($sql, array(\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY));
+		$st->execute($data);
+		return $st;
+	}
 	
 	
 }
